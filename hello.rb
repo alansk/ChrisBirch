586c4c7ec54eceb2e56207547b912282ab180bef
Alan Skerrett <alan.skerrett@gmail.com>
2011-12-11 09:55:18 +0000

    Basic db setup

--- hello.rb
+++ hello.rb
@@ -1,5 +1,23 @@
+require 'rubygems'
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Section
+  include DataMapper::Resource  
+  property :id,           Serial
+  property :name,         String
+  property :parentid, 	Integer
+end
+
+DataMapper.auto_upgrade!
+
+@post = Section.create(
+  :name      => "Ads"
+)
 
 get '/' do
-  "Chris Birch: Business Man"
+	@section = Section.get(1)
+  "Chris Birch: Business Man <br/> --- <br/> " + @section.name
 end
